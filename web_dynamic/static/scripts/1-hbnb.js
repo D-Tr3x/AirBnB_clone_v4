29285816a5cd4982f0efa8c4cef952e5d8add00f
D-Tr3x <owosenidaniel13@gmail.com>
2025-02-25 04:33:35 +0100

    Implemeted AJAX request to fetch places from /api/v1/places_search/, Send a `POST` request with an empty JSON body, Removed backend-rendered places loading them from the frontend

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,16 +1,17 @@
+/* global $ */
 $(document).ready(function () {
-  let amenities = {};
+  const amenities = {};
 
   $('input[type="checkbox"]').change(function () {
-      const amenityId = $(this).data('id');
-      const amenityName = $(this).data('name');
+    const amenityId = $(this).data('id');
+    const amenityName = $(this).data('name');
 
-      if ($(this).is(':checked')) {
-        amenities[amenityId] = amenityName;
-      } else {
-        delete amenities[amenityId];
-      }
+    if ($(this).is(':checked')) {
+      amenities[amenityId] = amenityName;
+    } else {
+      delete amenities[amenityId];
+    }
 
-      $('.amenities h4').text(Object.values(amenities).join(', '));
+    $('.amenities h4').text(Object.values(amenities).join(', '));
   });
 });
